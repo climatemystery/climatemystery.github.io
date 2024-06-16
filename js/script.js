1e49541632ef3fc1dda50ea00c9102ee784cad4e
Pratik Kad <pratikkad17@gmail.com>
2024-06-16 17:17:59 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+// script.js
+
+// Example: Handle form submission
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Here you can add code to handle form submission (e.g., send data to server)
+    alert('Form submitted!');
+});
 
