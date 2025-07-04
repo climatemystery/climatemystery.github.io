248a2a6a137977e99a3997b6ab7a2420bf63744a
Pratik Kad <pratikkad17@gmail.com>
2025-07-04 21:44:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// script.js - Example for smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+<script src="script.js"></script>
