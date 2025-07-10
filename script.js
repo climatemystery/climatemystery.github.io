e476177be1b1fe7da5ea3c4d8122cacda86f64d1
Pratik Kad <pratikkad17@gmail.com>
2025-07-11 01:05:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-<script src="script.js"></script>
+// script.js - Example for smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
